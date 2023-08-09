c9fbd10316c72859280e8d03292dac6ceff3b6df
guilhermebarroso-sys <guibarrosodeoliveira5@gmail.com>
2023-08-09 11:07:22 -0300

    fix: :bug: Fixing api baseUrl bug

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const api = typeof window == "undefined" ? axios.create({
-	baseURL: "http://testgui.beauty/api",
+	baseURL: "http://www.testgui.beauty/api",
 	headers: {
 		Host: "testgui.beauty"
 	}
 })
 	: 
-	axios.create({ baseURL: "http://testgui.beauty/api"});
+	axios.create({ baseURL: "http://www.testgui.beauty/api"});
 
 
 
