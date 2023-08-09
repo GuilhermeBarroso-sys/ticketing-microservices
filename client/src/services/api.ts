ebec3b731da302f7488049b825728ab83b64d612
guilhermebarroso-sys <guibarrosodeoliveira5@gmail.com>
2023-08-09 11:13:21 -0300

    Fixing server-side headers host

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = typeof window == "undefined" ? axios.create({
 	baseURL: "http://www.testgui.beauty/api",
 	headers: {
-		Host: "testgui.beauty"
+		Host: "www.testgui.beauty"
 	}
 })
 	: 
